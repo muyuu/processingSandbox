642173d9c749b6beb3c5fa4c83c44a532d6efa58
muyuu <anticyborg@gmail.com>
2015-07-21 17:33:09 +0900

    add preventDefault() from click event handler

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,14 @@
         $(".js-runSwitcher a").on("click", selectSketch);
     }
 
-    function selectSketch(){
+    function selectSketch(e){
+        e.preventDefault();
+
         var data = $(this).data("processing-sources");
         var pdePath = "./assets/pde/" + data + ".pde";
+
         canvas.attr({ "data-processing-sources": pdePath });
+
         Processing.reload();
     }
 
